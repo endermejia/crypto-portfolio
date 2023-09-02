91a89ee9a38f5f083ab4d2077a05fdbb75b6db68
endermejia <endermejia032@gmail.com>
2023-09-02 17:57:31 +0200

    feature (environment) config env

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,84 +2,94 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Coin, Portfolio, PortfolioLine} from './models/models';
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'http://localhost:3000';
+  private serverUrl = environment.serverUrl;
 
   constructor(
     private http: HttpClient
   ) {
   }
 
+  getEurValueByAcronym(acronym: string): Observable<{ EUR: number }> {
+    const cryptoValueUrl = `https://min-api.cryptocompare.com/data/price?fsym=${acronym}&tsyms=EUR`;
+    return this.http.get(cryptoValueUrl) as Observable<{ EUR: number }>;
+  }
+
+  getCryptoCompareData(): Observable<any> {
+    return this.http.get('https://min-api.cryptocompare.com/data/all/coinlist') as Observable<any>;
+  }
+
   // Currencies:
 
   getCurrencies(): Observable<Coin[]> {
-    return this.http.get(`${this.baseUrl}/coins`) as Observable<any[]>;
+    return this.http.get(`${this.serverUrl}/coins`) as Observable<Coin[]>;
   }
 
   getCurrency(currencyId: number): Observable<Coin> {
-    return this.http.get(`${this.baseUrl}/coins/${currencyId}`) as Observable<Coin>;
+    return this.http.get(`${this.serverUrl}/coins/${currencyId}`) as Observable<Coin>;
   }
 
   createCurrency(currency: Coin): Observable<Coin> {
-    return this.http.post(`${this.baseUrl}/coins`, currency) as Observable<Coin>;
+    return this.http.post(`${this.serverUrl}/coins`, currency) as Observable<Coin>;
   }
 
   updateCurrency(currency: Coin): Observable<Coin> {
-    return this.http.put(`${this.baseUrl}/coins/${currency.id}`, currency) as Observable<Coin>;
+    return this.http.put(`${this.serverUrl}/coins/${currency.id}`, currency) as Observable<Coin>;
   }
 
   deleteCurrency(currencyId: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/coins/${currencyId}`);
+    return this.http.delete(`${this.serverUrl}/coins/${currencyId}`);
   }
 
   // Portfolios:
 
   getPortfolios(): Observable<Portfolio[]> {
-    return this.http.get(`${this.baseUrl}/portfolios`) as Observable<Portfolio[]>;
+    return this.http.get(`${this.serverUrl}/portfolios`) as Observable<Portfolio[]>;
   }
 
   getPortfolio(portfolioId: number): Observable<Portfolio> {
-    return this.http.get(`${this.baseUrl}/portfolios/${portfolioId}`) as Observable<Portfolio>;
+    return this.http.get(`${this.serverUrl}/portfolios/${portfolioId}`) as Observable<Portfolio>;
   }
 
   createPortfolio(portfolio: Portfolio): Observable<Portfolio> {
-    return this.http.post(`${this.baseUrl}/portfolios`, portfolio) as Observable<Portfolio>;
+    return this.http.post(`${this.serverUrl}/portfolios`, portfolio) as Observable<Portfolio>;
   }
 
   updatePortfolio(portfolio: Portfolio): Observable<Portfolio> {
-    return this.http.put(`${this.baseUrl}/portfolios/${portfolio.id}`, portfolio) as Observable<Portfolio>;
+    return this.http.put(`${this.serverUrl}/portfolios/${portfolio.id}`, portfolio) as Observable<Portfolio>;
   }
 
   deletePortfolio(portfolioId: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/portfolios/${portfolioId}`);
+    return this.http.delete(`${this.serverUrl}/portfolios/${portfolioId}`);
   }
 
   // Portfolio lines:
 
   getPortfolioLines(portfolioId: number): Observable<PortfolioLine[]> {
-    return this.http.get(`${this.baseUrl}/portfolios/${portfolioId}/lines`) as Observable<PortfolioLine[]>;
+    return this.http.get(`${this.serverUrl}/portfolios/${portfolioId}/lines`) as Observable<PortfolioLine[]>;
   }
 
   getPortfolioLine(portfolioId: number, portfolioLineId: number): Observable<PortfolioLine> {
-    return this.http.get(`${this.baseUrl}/portfolios/${portfolioId}/lines/${portfolioLineId}`) as Observable<PortfolioLine>;
+    return this.http.get(`${this.serverUrl}/portfolios/${portfolioId}/lines/${portfolioLineId}`) as Observable<PortfolioLine>;
   }
 
   createPortfolioLine(portfolioId: number, portfolioLine: PortfolioLine): Observable<PortfolioLine> {
     portfolioLine.portfolioId = portfolioId;
-    return this.http.post(`${this.baseUrl}/lines`, portfolioLine) as Observable<PortfolioLine>;
+    return this.http.post(`${this.serverUrl}/lines`, portfolioLine) as Observable<PortfolioLine>;
   }
 
   updatePortfolioLine(portfolioId: number, portfolioLine: PortfolioLine): Observable<PortfolioLine> {
     portfolioLine.portfolioId = portfolioId;
-    return this.http.put(`${this.baseUrl}/lines/${portfolioLine.id}`, portfolioLine) as Observable<PortfolioLine>;
+    return this.http.put(`${this.serverUrl}/lines/${portfolioLine.id}`, portfolioLine) as Observable<PortfolioLine>;
   }
 
   deletePortfolioLine(portfolioLineId: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/lines/${portfolioLineId}`);
+    return this.http.delete(`${this.serverUrl}/lines/${portfolioLineId}`);
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-/// <reference types="@angular/localize" />
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 
-import { AppModule } from './app/app.module';
-
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
