c399a2beca340e68f34d4a9d0841e9d5203d02cb
endermejia <endermejia032@gmail.com>
2023-09-05 11:48:50 +0200

    fix (dialog) dialog component issues

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,34 +1,43 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {DialogService} from "../../services/dialog.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.css']
 })
-export class DialogComponent implements OnInit {
+export class DialogComponent implements OnInit, OnDestroy {
 
   title: string | undefined;
   message: string | undefined;
   showCancelButton = false;
+  subscriptions: Subscription[] = [];
 
   constructor(
     private dialogService: DialogService
   ) {
   }
 
-  ngOnInit() {
-    this.dialogService.dialogData.subscribe({
-      next: (data: { title: string, message: string, showCancelButton: boolean }) => {
-        this.title = data.title;
-        this.message = data.message;
-        this.showCancelButton = data.showCancelButton;
-        document.getElementById('buttonDialog')?.click();
-      },
-      error: (err) => {
-        console.error(err);
-      }
-    });
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.dialogService.dialogData.subscribe({
+        next: (data: { title: string, message: string, showCancelButton: boolean }) => {
+          debugger;
+          this.title = data.title;
+          this.message = data.message;
+          this.showCancelButton = data.showCancelButton;
+          document.getElementById('buttonDialog')?.click();
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 
   onConfirm() {
